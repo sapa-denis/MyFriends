b2f68cddbdcf272ad3620c7299bc12302903e9ee
Sapa Denys <sapa.denis@gmail.com>
2015-06-25 00:58:49 +0300

    add 'Friend' model

---
+++ MyFriends/Model/Friend.h
@@ -0,0 +1,22 @@
+//
+//  Friend.h
+//  
+//
+//  Created by Sapa Denys on 25.06.15.
+//
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+
+@interface Friend : NSManagedObject
+
+@property (nonatomic, retain) NSString * firstName;
+@property (nonatomic, retain) NSString * lastName;
+@property (nonatomic, retain) NSString * phone;
+@property (nonatomic, retain) NSString * email;
+@property (nonatomic, retain) NSData * photo;
+@property (nonatomic, retain) NSString * photoULRString;
+
+@end
---
+++ MyFriends/Model/Friend.m
@@ -0,0 +1,21 @@
+//
+//  Friend.m
+//  
+//
+//  Created by Sapa Denys on 25.06.15.
+//
+//
+
+#import "Friend.h"
+
+
+@implementation Friend
+
+@dynamic firstName;
+@dynamic lastName;
+@dynamic phone;
+@dynamic email;
+@dynamic photo;
+@dynamic photoULRString;
+
+@end
